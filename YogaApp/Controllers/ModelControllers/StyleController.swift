c3547cca9054fa44d8146db03bfd482c52c9f26b
Ashton <ashton.martin191@gmail.com>
2022-06-16 18:29:37 -0500

    uploaded app

---
+++ YogaApp/Controllers/ModelControllers/StyleController.swift
@@ -0,0 +1,16 @@
+//
+//  StyleController.swift
+//  YogaApp
+//
+//  Created by Ashton Martin on 6/16/22.
+//
+
+import UIKit
+
+class StyleController {
+    static let hatha = [
+        Style(name: "Hatha", description: "Hatha yoga is a branch of yoga primarily concerned with mastery of the body, and it is arguably the tradition most familiar to Western culture. The term is derived from the Sanskrit ha, meaning 'sun', and tha, meaning 'moon', leading to the common interpretation that Hatha practices are designed to unite and balance these two energies. However, the literal translation of Hatha is 'force', alluding to the physical techniques used within this tradition. Traditionally, Hatha yoga offered a total life philosophy, incorporating how we relate to external and internal experience as a path to inner peace. One of the most prominent ancient texts on Hatha yoga is Patanjali’s Yoga Sutras, a series of 196 aphorisms written in Sanskrit around 400 AD. Patanjali describes Hatha yoga as an eightfold path, consisting of eight mind-body disciplines to be mastered. All other physical practices of yoga such as Vinyasa, Ashtanga and Iyengar stemmed from the Hatha yoga tradition.", photo: UIImage(named: "hatha")),
+        Style(name: "Vinyasa", description: "Vinyasa is a type of yoga that links movement and breath to attain balance in the mind and body. From the Sanskrit “to place in a special way,” vinyasa aligns a deliberate sequence of poses with the breath to achieve a continuous flow. Inhalation is usually connected to upward, open movements, while exhalation is often tied to downward movements or twists. This term can denote any of the following: The link between movement and breath; A yoga sequence that exemplifies this movement/breath connection; A class that focuses on this type of yoga; or The act of setting an intention and taking steps to reach it..", photo: UIImage(named: "vinyasa")),
+        Style(name: "Ashtanga", description: "Ashtanga yoga, sometimes referred to as Ashtanga Vinyasa yoga, is a style of yoga that was developed by Sri K. Pattabhi Jois and T. Krishnamacharya in the 20th century. They claimed it originated from a system of Hatha yoga described in the ancient text, the 'Yoga Korunta'. Used in this context, the term, Ashtanga yoga, refers to this particular style of yoga. Ashtanga yoga is a dynamic, flowing style that connects the movement of the body with the breath. The method stresses the importance of daily practice of a set series of movements. There are six series of Ashtanga yoga sequences, which the student progresses through at their own pace.", photo: UIImage(named: "ashtanga"))
+    ]
+}
---
+++ YogaApp/Models/Style.swift
@@ -0,0 +1,22 @@
+//
+//  Style.swift
+//  YogaApp
+//
+//  Created by Ashton Martin on 6/16/22.
+//
+
+import UIKit
+
+class Style {
+    let name: String
+    let description: String
+    let photo: UIImage?
+    
+    init(name: String, description: String, photo: UIImage?){
+        self.name = name
+        self.description = description
+        self.photo = photo
+    }
+}
+
+//xextension 
