e7c9253d4595eef0e33ce12dd44465c1054b4097
Ashton <ashton.martin191@gmail.com>
2022-06-16 16:56:01 -0500

    Initial Commit

---
+++ YogaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YogaApp
+//
+//  Created by Ashton Martin on 6/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
